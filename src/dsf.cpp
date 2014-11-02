2efdc472522929935096371b4ba94a83f3798a50
Thomas Ostler <tomostler13@googlemail.com>
2014-11-02 10:59:34 +0000

    Started to add files for calculating the DSF.

---
+++ src/dsf.cpp
@@ -0,0 +1,28 @@
+// File: dsf.cpp
+// Author:Tom Ostler
+// Created: 2 Nov 2014
+// Last-modified: 02 Nov 2014 10:59:13
+#include "../inc/config.h"
+#include "../inc/error.h"
+#include "../inc/geom.h"
+#include "../inc/util.h"
+#include "../inc/arrays.h"
+#include "../inc/unitcell.h"
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#define FIXOUT(a,b) a.width(75);a << std::left << b;
+#define FIXOUTVEC(a,b,c,d,e) FIXOUT(a,b << "[   ");a.width(5);a << std::left << c << " , ";a.width(5);a << std::left << d << " , ";a.width(5);a << std::left << e << "   ]" << std::endl;
+
+//The aim of this part of the code is to take the user input
+//and calculate the dynammic structure factor based on what
+//the user selects. The actual calculations should not be
+//specific to any one calculations and therefore have their
+//own control features in the config file dsf:
+namespace dsf
+{
+    void
+
+}
