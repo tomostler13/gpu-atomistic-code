18ea9111d51457aa09ecd7cb83bf9e058ffbf9f8
Thomas Ostler <tomostler13@googlemail.com>
2014-12-05 11:47:24 +0000

    In the middle of tidying up the exchnage files. The code is not really in a working state at the moment.

--- inc/exch.h
+++ inc/exch.h
@@ -1,20 +1,29 @@
 // File: exch.h
 // Author:Tom Ostler
 // Created: 18 Jan 2013
-// Last-modified: 11 Nov 2014 13:58:00
+// Last-modified: 05 Dec 2014 11:18:36
 #ifndef _EXCH_H_
 #define _EXCH_H_
+#include <string>
 #include "../inc/arrays.h"
 namespace exch
 {
-    extern Array<int> diagoffset,offdiagoffset;
+    extern Array<int> diagoffset,offdiagoffset,checkdiag;
     extern Array<unsigned int> xadj,adjncy,offdiagxadj,offdiagadjncy;
     extern Array<double> dataxx,dataxy,dataxz,datayx,datayy,datayz,datazx,datazy,datazz;
+    extern Array<double> evs;
+    extern Array2D<unsigned int> shell_list;
     extern Array3D<unsigned int> numint;
     extern Array4D<double> exchvec;
     extern Array5D<double> J;
-    extern unsigned int num_shells,diagnumdiag,offdiagnumdiag;
-    extern bool outputJ;
+    extern unsigned int num_shells,diagnumdiag,offdiagnumdiag,max_shells;
+    extern bool outputJ,oem,rem;
+    extern std::string readMethod,readFile,method,enerType;
     void initExch(int argc,char *argv[]);
+    void readGlobalExch(int argc,char *argv[]);
+
+    extern libconfig::Config exchcfg;
+    void permute();
+    void direct();
 }
 #endif /*_EXCH_H_*/
---
+++ src/exch_determine_exchange_matrix.cpp
@@ -0,0 +1,7 @@
+// File: exch_determine_exchange_matrix.cpp
+// Author: Tom Ostler
+// Created: 05 Dec 2014
+// Last-modified: 05 Dec 2014 11:46:46
+// This source file was added to tidy up the file exch.cpp
+// because it was becoming cumbersome to work with. This
+// source file calculates the CSR neighbourlist
